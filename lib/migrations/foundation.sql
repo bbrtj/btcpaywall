b2c5cc48a3cfbcd70fd7d4e7d362e2725f9527d5
brtastic <brtastic.dev@gmail.com>
2021-06-18 23:53:34 +0200

    Add accounts to migration

--- lib/migrations/foundation.sql
+++ lib/migrations/foundation.sql
@@ -1,18 +1,32 @@
 -- 1 up
 
+CREATE SEQUENCE bip44_accounts MINVALUE 0 MAXVALUE 2147483647 START 0;
 CREATE SEQUENCE bip44_indexes MINVALUE 0 MAXVALUE 2147483647 START 0;
+
+CREATE TABLE accounts (
+	id uuid primary key,
+	account_index INT NOT NULL DEFAULT nextval('bip44_accounts'),
+	secret VARCHAR(32) NOT NULL
+);
 
 CREATE TABLE requests (
 	id uuid primary key,
+	account_id uuid NOT NULL,
 	amount INT NOT NULL,
 	derivation_index INT NOT NULL DEFAULT nextval('bip44_indexes'),
 	status VARCHAR(32) NOT NULL DEFAULT 'awaiting',
-	ts TIMESTAMP NOT NULL DEFAULT NOW()
+	ts TIMESTAMP NOT NULL DEFAULT NOW(),
+	CONSTRAINT fk_account_id
+		FOREIGN KEY(account_id)
+		REFERENCES accounts(id)
 );
 
 CREATE UNIQUE INDEX uniq_requests_derivation_index ON requests (derivation_index);
-CREATE INDEX ind_requests_status ON requests (status);
+CREATE INDEX ind_requests_lookup ON requests (status, account_id);
 
 -- 1 down
 
 DROP TABLE requests;
+DROP TABLE accounts;
+DROP SEQUENCE bip44_indexes;
+DROP SEQUENCE bip44_accounts;
