bfac7dd52c480ce4218c5d91a6adbfde1770cdc1
brtastic <brtastic.dev@gmail.com>
2021-08-11 16:35:16 +0200

    Format address on the page to be more easily readable

--- lib/Helpers.pm
+++ lib/Helpers.pm
@@ -8,3 +8,13 @@ sub satoshi_to_bitcoin ($amount)
 {
 	return sprintf('%.8f', $amount / Constants::SATOSHI_PER_BITCOIN);
 }
+
+sub format_address ($address, $group_size = 6)
+{
+	my @groups;
+	while (length $address > 0) {
+		push @groups, substr $address, 0, $group_size, '';
+	}
+
+	return join ' ', @groups;
+}
