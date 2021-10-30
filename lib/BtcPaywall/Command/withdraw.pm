4852c90be02cbbe51484c935905e28c77615aa6e
brtastic <brtastic.dev@gmail.com>
2021-10-30 18:14:25 +0200

    Add dedicated CLI action to withdraw funds

---
+++ lib/BtcPaywall/Command/withdraw.pm
@@ -0,0 +1,35 @@
+package BtcPaywall::Command::withdraw;
+
+use Mojo::Base 'Mojolicious::Command';
+use DI;
+
+use header;
+
+has description => 'Withdraw all available coins to an address';
+
+# Usage message from SYNOPSIS
+has usage => sub ($self) { $self->extract_usage };
+
+sub run ($self, $address)
+{
+	my $requests = DI->get('requests_unit_repository')->find(
+		status => [
+			Model::Request->STATUS_COMPLETE,
+			Model::Request->STATUS_CALLBACK_FAILED,
+		]
+	);
+
+	my $node = DI->get('node');
+	my $mkey = DI->get('master_key');
+	for my $unit ($requests->@*) {
+		$node->import_private_key($mkey->reveal_key($unit->account, $unit->request));
+	}
+
+	$node->withdraw($address);
+	say 'Withdrawal complete';
+}
+
+__END__
+=head1 SYNOPSIS
+
+	Usage: APPLICATION withdraw [ADDRESS]
