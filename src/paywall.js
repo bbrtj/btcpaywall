2d44f72396f72537cfa3c36c3a6a40651369c4d8
brtastic <brtastic.dev@gmail.com>
2021-11-12 06:18:07 +0100

    Add proper copying of the address through javascript
    Copy the unformatted address, without the spaces added for readability

--- src/paywall.js
+++ src/paywall.js
@@ -1,2 +1,12 @@
 import "./styles.scss";
 import "bootstrap";
+
+document.addEventListener('DOMContentLoaded', (event) => {
+	let el = document.getElementById('address-field');
+
+	el.addEventListener('copy', (e) => {
+		let address = el.getAttribute('data-address');
+
+		navigator.clipboard.writeText(address);
+	});
+});
